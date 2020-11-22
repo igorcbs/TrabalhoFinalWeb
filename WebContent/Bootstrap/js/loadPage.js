34e2a5413893c751fc7b6c442e959f222f79539e
Victor Dângelo <terrasterd@gmail.com>
2020-11-22 00:37:39 -0300

    finalizando gerar divs

--- WebContent/Bootstrap/js/loadPage.js
+++ WebContent/Bootstrap/js/loadPage.js
@@ -2,10 +2,11 @@
        window.location.href=pageUrl;
      }
 
-function adada(){
+function loadItens(){
 	var counts = ['1','2','3','4'];
-	var container = document.getElementById('jogos');
+	document.setParameter("nome", Overwatch)
+	var container = document.getElementById('divPrincipal');
 	counts.forEach(function(count) {
-	    container.append('<div>' + count + '</div>');
+	    container.append('<div class="col-md-6"><div class="review-item"><div class="review-cover set-bg" data-setbg="Bootstrap/img/review/5.jpg"></div><div class="review-text"><h4>Overwatch Halloween</h4><p>Multiplayer de 4 jogadores</p></div></div></div>');
 	});
 }
